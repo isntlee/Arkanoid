1e91c48c3a85ecd60b6d0888911eb7f4dcd683af
Space-Rx <leedavidmartina@gmail.com>
2020-03-19 21:24:53 +0000

    First commit; built in main()

---
+++ arkanoid.py
@@ -0,0 +1,13 @@
+import sys, pygame, random 
+
+
+class Breakout():
+
+    def main(self):
+        xspeed_init = 6
+        yspeed_init = 6
+        max_lives = 5
+        bat_speed = 30 
+        score = 0 
+        bgcolour = 0, 0, 0 
+        size = width, height = 720, 640 
