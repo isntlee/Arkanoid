3b4549ac871bcf3e8b20aa4e11311188bac97c53
Space-Rx <leedavidmartina@gmail.com>
2020-03-20 19:39:54 +0000

    added Wall class, and main()

--- arkanoid.py
+++ arkanoid.py
@@ -147,4 +147,35 @@ class Arkanoid():
             screen.blit(bat, batrect)
             pygame.display.flip()
 
+
+class Wall():
+
+    def __init__ (self): 
+        self.brick = pygame.image.load("brick.png").convert()
+        brickrect = self.brick.get_rect()
+        self.bricklength = brickrect.right - brickrect.left
+        self.brickheight = brickrect.bottom - brickrect.loop 
+    
+    def build_wall(self, width):
+        xpos = 0
+        ypos = 0 
+        adj = 0 
+        self.brickrect = []
+        for i in range (0, 116):
+            if xpos > width:
+                if adj == 0:
+                    adj = self.bricklength/2
+                else:
+                    adj = 0
+                xpos = -adj
+                ypos += self.brickheight
+
+            self.brickrect.append(self.brick.get_rect())
+            self.brickrect[i] = self.brickrect[i].move(xpos, ypos)
+            xpos = xpos + self.bricklength
+
+if __name__ == "__main__":
+    br = Arkanoid()
+    br.main()
+
             
